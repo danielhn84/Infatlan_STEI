8e097e3dd9a180ad737cdfa71bb7ea9d8f62ccdb
Wendel Joseph Padilla Vega <wpadilla@bancatlan.hn>
2020-02-12 11:22:41 -0600

    Acrtualizacion 12.02.2020 conexion db

---
+++ Infatlan_STEI_Inventario/clases/LdapService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace Infatlan_STEI_Inventario.clases
+{
+    public class LdapService
+    {
+        public LdapService() { }
+
+        public bool ValidateCredentials(string domain, string username, string password)
+        {
+            using (var context = new PrincipalContext(ContextType.Domain, domain))
+            {
+                return context.ValidateCredentials(username, password);
+            }
+        }
+
+        public bool IsUserInAdGroup(string domain, string username, string adGroupName)
+        {
+            bool result = false;
+            using (var context = new PrincipalContext(ContextType.Domain, domain))
+            {
+                var user = UserPrincipal.FindByIdentity(context, username);
+                if (user != null)
+                {
+                    var group = GroupPrincipal.FindByIdentity(context, adGroupName);
+                    if (group != null && user.IsMemberOf(group))
+                        result = true;
+                }
+            }
+            return result;
+        }
+
+        public String[] GetGroups(String username, String password)
+        {
+            String vDomain = Domain.GetCurrentDomain().Name;
+            var allRoles = new List<string>();
+            var root = new DirectoryEntry(vDomain, username, password);
+            var searcher = new DirectorySearcher(root, string.Format(CultureInfo.InvariantCulture, "(&(objectClass=user)({0}={1}))", "samAccountName", username));
+
+            searcher.PropertiesToLoad.Add("memberOf");
+            SearchResult result = searcher.FindOne();
+            if (result != null && !string.IsNullOrEmpty(result.Path))
+            {
+                DirectoryEntry user = result.GetDirectoryEntry();
+                PropertyValueCollection groups = user.Properties["memberOf"];
+                foreach (string path in groups)
+                {
+                    string[] parts = path.Split(',');
+                    if (parts.Length > 0)
+                    {
+                        foreach (string part in parts)
+                        {
+                            string[] p = part.Split('=');
+                            if (p[0].Equals("cn", StringComparison.OrdinalIgnoreCase))
+                            {
+                                allRoles.Add(p[1]);
+                            }
+                        }
+                    }
+                }
+            }
+            return allRoles.ToArray();
+        }
+
+    }
+}
---
+++ Infatlan_STEI_Inventario/clases/SmtpService.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+
+namespace BiometricoWeb.clases
+{
+    public enum typeBody{
+        Supervisor,
+        RecursosHumanos,
+        Solicitante,
+        Aprobado,
+        Rechazado,
+        Token,
+        Seguridad
+    }
+
+    public class SmtpService : Page{
+
+        public SmtpService() { }
+
+        public Boolean EnviarMensaje(String To, typeBody Body, String Usuario, String Nombre){
+            Boolean vRespuesta = false;
+            try{
+                MailMessage mail = new MailMessage("Recursos Humanos<" + ConfigurationManager.AppSettings["SmtpFrom"] + ">", To);
+                SmtpClient client = new SmtpClient();
+                client.Port = Convert.ToInt32(ConfigurationManager.AppSettings["SmtpPort"]);
+                //client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = false;
+                client.Host = ConfigurationManager.AppSettings["SmtpServer"];
+                mail.Subject = "Recursos Humanos - Información de empleado";
+                mail.IsBodyHtml = true;
+
+                switch (Body){
+                    case typeBody.Supervisor:
+                        mail.AlternateViews.Add(CreateHtmlMessage(PopulateBody(
+                            Usuario,
+                            "El empleado " + Nombre + " ha creado una petición de permiso.",
+                            ConfigurationManager.AppSettings["Host"] + "/pages/authorizations.aspx",
+                            "Te informamos que el permiso tiene que ser autorizado, para que sea procesado por Recursos Humanos."
+                            ), Server.MapPath("/images/logo.png")));
+                        break;
+                    case typeBody.RecursosHumanos:
+                        mail.AlternateViews.Add(CreateHtmlMessage(PopulateBody(
+                            Usuario,
+                            "El empleado " + Nombre + " ha creado una petición de permiso.",
+                            ConfigurationManager.AppSettings["Host"] + "/pages/authorizations.aspx",
+                            "Te informamos que el permiso ha sido autorizado por el jefe del area, ingresa al link para poder ver mas detalles."
+                            ), Server.MapPath("/images/logo.png")));
+                        break;
+                    case typeBody.Solicitante:
+                        mail.AlternateViews.Add(CreateHtmlMessage(PopulateBody(
+                            Usuario,
+                            "Has creado una petición de permiso.",
+                            ConfigurationManager.AppSettings["Host"] + "/pages/permissions.aspx",
+                            "Te informamos que el permiso ha sido enviado a tu jefe para la debida autorización."
+                            ), Server.MapPath("/images/logo.png")));
+                        break;
+                    case typeBody.Aprobado:
+                        mail.AlternateViews.Add(CreateHtmlMessage(PopulateBody(
+                            Usuario,
+                            "Se ha aprobado tu solicitud de permiso",
+                            ConfigurationManager.AppSettings["Host"] + "/pages/permissions.aspx",
+                            "Te informamos que el permiso que has solicitado ha sido aprobado."
+                            ), Server.MapPath("/images/logo.png")));
+                        break;
+                    case typeBody.Rechazado:
+                        mail.AlternateViews.Add(CreateHtmlMessage(PopulateBody(
+                            Usuario,
+                            "Se ha rechazado tu solicitud de permiso",
+                            ConfigurationManager.AppSettings["Host"] + "/pages/permissions.aspx",
+                            Nombre
+                            ), Server.MapPath("/images/logo.png")));
+                        break;
+                    case typeBody.Token:
+                        mail.AlternateViews.Add(CreateHtmlMessage(PopulateBody(
+                            Usuario,
+                            "Se ha creado un Token para emergencias",
+                            ConfigurationManager.AppSettings["Host"] + "/pages/permissions.aspx",
+                            "Token: <b>" + Nombre + @"</b> <br \> Ingresa este token en la pagina de permisos despues de haber seleccionado la opción de emergencias, este token solo puede ser utilizado una sola vez."
+                            ), Server.MapPath("/images/logo.png")));
+                        break;
+                    case typeBody.Seguridad:
+                        string[] vInfo = Nombre.Split('-');
+
+                        mail.AlternateViews.Add(CreateHtmlMessage(PopulateBodyES(
+                            Usuario,
+                            "Se ha creado un registro de <b>" + vInfo[0].ToString() + "</b> con su autorización. " +
+                            "Id: <b>" + vInfo[1].ToString() + "</b><br> Si no ha autorizado la salida del artículo. Favor comuníquese con el personal de seguridad."
+                            ), Server.MapPath("/images/logo.png")));
+                        break;
+
+                }
+                client.Send(mail);
+                vRespuesta = true;
+            }catch (System.Net.Mail.SmtpException Ex){
+                String vError = Ex.Message;
+                throw;
+            }catch (Exception Ex){
+                throw;
+            }
+            return vRespuesta;
+        }
+        
+        private AlternateView CreateHtmlMessage(string message, string logoPath){
+            var inline = new LinkedResource(logoPath, "image/png");
+            inline.ContentId = "companyLogo";
+
+            var alternateView = AlternateView.CreateAlternateViewFromString(
+                                    message,
+                                    Encoding.UTF8,
+                                    MediaTypeNames.Text.Html);
+            alternateView.LinkedResources.Add(inline);
+
+            return alternateView;
+        }
+
+        public string PopulateBody(string vNombre, string vTitulo, string vUrl, string vDescripcion){
+            string body = string.Empty;
+            using (StreamReader reader = new StreamReader(Server.MapPath("/pages/mail/TemplateMail.html"))){
+                body = reader.ReadToEnd();
+            }
+
+            body = body.Replace("{Host}", ConfigurationManager.AppSettings["Host"]);
+            body = body.Replace("{Nombre}", vNombre);
+            body = body.Replace("{Titulo}", vTitulo);
+            body = body.Replace("{Url}", vUrl);
+            body = body.Replace("{Descripcion}", vDescripcion);
+            return body;
+        }
+
+        public string PopulateBodyES(string vNombre, string vDescripcion){
+            string body = string.Empty;
+            using (StreamReader reader = new StreamReader(Server.MapPath("/pages/mail/TemplateMail.html"))){
+                body = reader.ReadToEnd();
+            }
+
+            body = body.Replace("{Host}", ConfigurationManager.AppSettings["Host"]);
+            body = body.Replace("{Nombre}", vNombre);
+            body = body.Replace("{Titulo}", "");
+            body = body.Replace("{Descripcion}", vDescripcion);
+            return body;
+        }
+    }
+}
