e08697158d89637b16299528ca3a1ce178a2b60a
Daniel Henriquez <dhenriqueza@hotmail.com>
2020-03-11 16:22:13 -0600

    Actualizacion de roles

---
+++ Infatlan_STEI/classes/roles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Infatlan_STEI.classes
+{
+
+    public class roles
+    {
+        public String Nombre { get; set; }
+        public String Usuario { get; set; }
+        public String Correo { get; set; }
+        public String Telefono { get; set; }
+        public String Identidad { get; set; }
+        public String Departamento { get; set; }
+        public rolAplicacion[] Aplicaciones { get; set; }
+    }
+    public class rolAplicacion
+    {
+        public String NombreAplicacion { get; set; }
+        public int Aplicacion { get; set; }
+        public int escritura { get; set; }
+        public int borrar { get; set; }
+        public int edicion { get; set; }
+        public int consulta { get; set; }
+    }
+
+    public class getRoles
+    {
+        public int Escritura { get; set; }
+        public int Edicion { get; set; }
+        public int Borrar { get; set; }
+        public int Consulta { get; set; }
+    }
+}
--- Infatlan_STEI/default.aspx.cs
+++ Infatlan_STEI/default.aspx.cs
@@ -12,6 +12,59 @@ namespace Infatlan_STEI
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            classes.rolAplicacion[] vRolAplicacion = new classes.rolAplicacion[2];
+            vRolAplicacion[0] = new classes.rolAplicacion()
+            { 
+                NombreAplicacion = "Agencias",
+                Aplicacion = 1,
+                escritura = 1,
+                edicion = 0,
+                consulta = 1,
+                borrar = 0
+            };
+            vRolAplicacion[1] = new classes.rolAplicacion()
+            {
+                NombreAplicacion = "ATMs",
+                Aplicacion = 2,
+                escritura = 1,
+                edicion = 0,
+                consulta = 1,
+                borrar = 0
+            };
+
+            classes.roles vRol = new classes.roles()
+            {
+                Nombre = "Daniel Henriquez",
+                Usuario = "dehenriquez",
+                Correo = "dehenriquez@hotmail.com",
+                Telefono = "94767348",
+                Identidad = "0801198406577",
+                Departamento = "8",
+                Aplicaciones = vRolAplicacion
+            };
+
+            Session["ROL"] = vRol;
+            getRol();
         }
+
+
+        public void getRol()
+        {
+            classes.roles vRol = (classes.roles)Session["ROL"];
+            classes.getRoles vRolesAplicacion = new classes.getRoles();
+            classes.generales vGenerales = new classes.generales();
+            Boolean vTieneAcceso = vGenerales.getAccess(1, (classes.roles)Session["ROL"], ref vRolesAplicacion);
+
+            if (!vTieneAcceso)
+                Response.Redirect("../default.aspx");
+
+
+            if (!vRolesAplicacion.Escritura.Equals(1))
+            {
+                throw new Exception("");
+            }
+
+        }
+
     }
 }
