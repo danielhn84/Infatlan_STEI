7147cb499ac458946651c4abfd683283b6d29040
Wendel Joseph Padilla Vega <wpadilla@bancatlan.hn>
2020-10-08 11:53:47 -0600

    Update 08.10.2020 - Page_Init del Login

--- Infatlan_STEI/login.aspx.cs
+++ Infatlan_STEI/login.aspx.cs
@@ -4,38 +4,19 @@ using System.Data;
 using System.Web.UI;
 using System.Web.Helpers;
 
-
 namespace Infatlan_STEI
 {
     public partial class login : System.Web.UI.Page
     {
         db vConexion = new db();
-
-        protected void Page_Init(object sender, EventArgs e)
-        {
-            try
-            {
-                MyDiv.InnerHtml = AntiForgery.GetHtml().ToString();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
         protected void Page_Load(object sender, EventArgs e){
             try{
-               
-                //if (IsPostBack)
 
-
-                //   AntiForgery.Validate();
-
-            }
-            catch (Exception ex){
+            }catch (Exception ex){
                 throw new Exception(ex.Message);
             }
         }
+
         protected void BtnLogin_Click(object sender, EventArgs e){
             try{
                 //System.Web.Helpers.AntiForgery.Validate();
