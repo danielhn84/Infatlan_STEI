e666b7a0c44c25ac55b546a02d1bdf6d5a8c09ec
Ana Carolina Amador Mejia <acamador@bancatlan.hn>
2020-11-06 09:49:19 -0600

    Se agrego nuevo modulo Gestiones Tecnicas

--- Infatlan_STEI/main.Master.designer.cs
+++ Infatlan_STEI/main.Master.designer.cs
@@ -114,6 +114,15 @@ namespace Infatlan_STEI
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl LIComunicaciones;
 
         /// <summary>
+        /// Control LIGestionesTec.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl LIGestionesTec;
+
+        /// <summary>
         /// Control LiCumplimiento.
         /// </summary>
         /// <remarks>
